f171ed78b26080d9912b7adf90243a8c15e6cd15
mdsahajeduddinsahed-cpu <mdsahajeduddinsahed@gmail.com>
2026-03-09 08:45:59 -0700

    rendering card ,dynamic border and priority style ,labels is ready used js

--- script.js
+++ script.js
@@ -12,3 +12,46 @@ function handleLogin() {
                 document.getElementById('error').classList.remove('hidden');
             }
         }
+// --- Load Data ---
+async function loadData() {
+    try {
+        const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+        const json = await res.json();
+        allData = json.data;
+        renderCards(allData);
+    } catch (error) {
+        console.error("Error loading data:", error);
+    }
+}
+
+// --- Rendering Card ---
+function renderCards(issues) {
+    const container = document.getElementById("issueContainer");
+    container.innerHTML = "";
+
+    issues.forEach(issue => {
+        // Dynamic Border Class
+        const border = issue.status === "open" 
+            ? "border-t-4 border-green-500" 
+            : "border-t-4 border-purple-500";
+
+        // Dynamic Priority Styles
+        const priorityColor = issue.priority === "high" 
+            ? "bg-red-100 text-red-500" 
+            : issue.priority === "medium" 
+                ? "bg-orange-100 text-orange-500" 
+                : "bg-gray-200 text-gray-500";
+
+        // Labels Generation
+        const labels = issue.labels.map(label => {
+            if (label === "bug") {
+                return `<span class="px-3 py-1 border border-red-400 text-red-500 bg-red-50 rounded-full text-xs"><i class="fa-solid fa-bug"></i> BUG</span>`;
+            }
+            if (label === "help wanted") {
+                return `<span class="px-3 py-1 border border-yellow-400 text-yellow-600 bg-yellow-50 rounded-full text-xs"><i class="fa-solid fa-life-ring"></i> HELP WANTED</span>`;
+            }
+            if (label === "enhancement") {
+                return `<span class="px-3 py-1 border border-green-400 text-green-600 bg-green-50 rounded-full text-xs">ENHANCEMENT</span>`;
+            }
+            return "";
+        }).join("");
